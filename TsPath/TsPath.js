1207d4ec8b407bec35d459359576bceb4df4c3ea
Dave Fancher <dave@davefancher.com>
2014-04-22 02:37:34 -0400

    Initial commit
    Created project structure and implemented a few commands

---
+++ TsPath/TsPath.js
@@ -0,0 +1,288 @@
+﻿var TsPath;
+(function (TsPath) {
+    var Point = (function () {
+        function Point(x, y) {
+            this._xCoordinate = x;
+            this._yCoordinate = y;
+        }
+        Object.defineProperty(Point.prototype, "X", {
+            get: function () {
+                return this._xCoordinate;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Point.prototype, "Y", {
+            get: function () {
+                return this._yCoordinate;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Point;
+    })();
+
+    var FillRule;
+    (function (FillRule) {
+        FillRule[FillRule["EvenOdd"] = 0] = "EvenOdd";
+        FillRule[FillRule["NonZero"] = 1] = "NonZero";
+    })(FillRule || (FillRule = {}));
+
+    var FillRuleCommand = (function () {
+        function FillRuleCommand(rule) {
+            this._fillRule = rule;
+        }
+        Object.defineProperty(FillRuleCommand.prototype, "FillRule", {
+            get: function () {
+                return this._fillRule;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        FillRuleCommand.prototype.Invoke = function (context) {
+            context.msFillRule = this._fillRule === 0 /* EvenOdd */ ? "evenodd" : "nonzero";
+        };
+        return FillRuleCommand;
+    })();
+
+    var MoveToCommand = (function () {
+        function MoveToCommand(p) {
+            this._point = p;
+        }
+        Object.defineProperty(MoveToCommand.prototype, "Point", {
+            get: function () {
+                return this._point;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        MoveToCommand.prototype.Invoke = function (context) {
+            context.moveTo(this._point.X, this.Point.Y);
+        };
+        return MoveToCommand;
+    })();
+
+    var LineToCommand = (function () {
+        function LineToCommand(p) {
+            this._point = p;
+        }
+        Object.defineProperty(LineToCommand.prototype, "Point", {
+            get: function () {
+                return this._point;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        LineToCommand.prototype.Invoke = function (context) {
+            context.lineTo(this._point.X, this.Point.Y);
+        };
+        return LineToCommand;
+    })();
+
+    var ClosePathCommand = (function () {
+        function ClosePathCommand() {
+        }
+        ClosePathCommand.prototype.Invoke = function (context) {
+            context.closePath();
+        };
+        return ClosePathCommand;
+    })();
+
+    var TextStream = (function () {
+        function TextStream(source) {
+            this._source = source;
+            this.Reset();
+        }
+        Object.defineProperty(TextStream.prototype, "Current", {
+            get: function () {
+                return this._position >= this._source.length ? null : this._source[this._position];
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        TextStream.prototype.MoveNext = function () {
+            return ++this._position < this._source.length;
+        };
+        TextStream.prototype.Reset = function () {
+            this._position = 0;
+        };
+        return TextStream;
+    })();
+
+    var PathParser = (function () {
+        function PathParser() {
+            this._wsChars = [" ", "\t"];
+            this._commandChars = ["M", "L", "C", "Q", "A", "Z"];
+        }
+        PathParser.prototype.IsCommandCharacter = function (current) {
+            return this._commandChars.indexOf(current) !== -1;
+        };
+
+        PathParser.prototype.IsWhitespaceCharacter = function (current) {
+            return this._wsChars.indexOf(current) !== -1;
+        };
+
+        PathParser.prototype.IsComma = function (current) {
+            return current === ",";
+        };
+
+        PathParser.prototype.IsNumberOrDecimal = function (current) {
+            return !isNaN(parseInt(current, 10)) || current === ".";
+        };
+
+        PathParser.prototype.SkipWhitespace = function (stream) {
+            while (this.IsWhitespaceCharacter(stream.Current) && stream.MoveNext()) {
+            }
+        };
+
+        PathParser.prototype.SkipComma = function (stream) {
+            if (this.IsComma(stream.Current))
+                stream.MoveNext();
+        };
+
+        PathParser.prototype.ReadNumber = function (stream) {
+            var numStr = "";
+
+            while (this.IsNumberOrDecimal(stream.Current)) {
+                numStr += stream.Current;
+                if (!stream.MoveNext())
+                    break;
+            }
+
+            return Number(numStr);
+        };
+
+        PathParser.prototype.ReadPoint = function (stream) {
+            var x = this.ReadNumber(stream);
+            this.SkipWhitespace(stream);
+            this.SkipComma(stream);
+            this.SkipWhitespace(stream);
+            var y = this.ReadNumber(stream);
+
+            return new Point(x, y);
+        };
+
+        PathParser.prototype.ParseFillStyleCommand = function (stream) {
+            if (!stream.MoveNext())
+                throw "Unexpected end of stream while parsing fill style command";
+
+            var fillRule;
+
+            switch (this.ReadNumber(stream)) {
+                case 0:
+                    fillRule = 0 /* EvenOdd */;
+                    break;
+                case 1:
+                    fillRule = 1 /* NonZero */;
+                    break;
+                default:
+                    throw "Invalid fill style option. Valid options are 0 (evenodd) and 1 (nonzero)";
+            }
+
+            this.SkipWhitespace(stream);
+
+            return [new FillRuleCommand(fillRule)];
+        };
+
+        PathParser.prototype.ParseMoveToCommand = function (stream) {
+            if (!stream.MoveNext())
+                throw "Unexpected end of stream while parsing move to command";
+            this.SkipWhitespace(stream);
+
+            var p = this.ReadPoint(stream);
+
+            this.SkipWhitespace(stream);
+
+            return [new MoveToCommand(p)];
+        };
+
+        PathParser.prototype.ParseLineToCommand = function (stream) {
+            if (!stream.MoveNext())
+                throw "Unexpected end of stream while parsing line to command";
+            this.SkipWhitespace(stream);
+
+            var commands = [];
+
+            while (!this.IsCommandCharacter(stream.Current) && stream.Current != null) {
+                var p = this.ReadPoint(stream);
+                this.SkipWhitespace(stream);
+                commands.push(new LineToCommand(p));
+            }
+
+            return commands;
+        };
+
+        PathParser.prototype.ParseClosePathCommand = function (stream) {
+            stream.MoveNext();
+            this.SkipWhitespace(stream);
+
+            return [new ClosePathCommand()];
+        };
+
+        PathParser.prototype.ResolveInitialCommand = function (stream) {
+            this.SkipWhitespace(stream);
+
+            switch (stream.Current) {
+                case "F":
+                    return this.ParseFillStyleCommand(stream);
+                case "M":
+                    return this.ParseMoveToCommand(stream);
+            }
+
+            throw "Invalid command";
+        };
+
+        PathParser.prototype.ResolveDrawingCommands = function (stream) {
+            switch (stream.Current) {
+                case "M":
+                    return this.ParseMoveToCommand(stream);
+                case "L":
+                    return this.ParseLineToCommand(stream);
+                case "C":
+                    return [];
+                case "Q":
+                    return [];
+                case "A":
+                    return [];
+                case "Z":
+                    return this.ParseClosePathCommand(stream);
+            }
+
+            throw "Invalid command";
+        };
+
+        PathParser.prototype.Parse = function (context, path) {
+            if (context === null)
+                throw "Missing drawing context";
+            if (path === "")
+                return [];
+
+            var stream = new TextStream(path);
+
+            this.ResolveInitialCommand(stream).forEach(function (c) {
+                return c.Invoke(context);
+            });
+
+            while (true) {
+                this.ResolveDrawingCommands(stream).forEach(function (c) {
+                    return c.Invoke(context);
+                });
+
+                if (stream.Current != null && this.IsCommandCharacter(stream.Current))
+                    continue;
+                if (!stream.MoveNext())
+                    break;
+            }
+
+            context.stroke();
+            //context.fill();
+        };
+        return PathParser;
+    })();
+    TsPath.PathParser = PathParser;
+})(TsPath || (TsPath = {}));
+//# sourceMappingURL=TsPath.js.map
---
+++ TsPath/TsPath.ts
@@ -0,0 +1,238 @@
+﻿module TsPath {
+    export interface IPathCommand {
+        Invoke(context: CanvasRenderingContext2D): void;
+    }
+
+    class Point {
+        private _xCoordinate: number;
+        private _yCoordinate: number;
+
+        get X(): number { return this._xCoordinate; }
+        get Y(): number { return this._yCoordinate; }
+
+        constructor(x: number, y: number) {
+            this._xCoordinate = x;
+            this._yCoordinate = y;
+        }
+    }
+
+    enum FillRule {
+        EvenOdd = 0,
+        NonZero = 1
+    }
+
+    class FillRuleCommand implements IPathCommand {
+        private _fillRule: FillRule;
+
+        get FillRule(): FillRule { return this._fillRule; }
+
+        constructor(rule: FillRule) {
+            this._fillRule = rule;
+        }
+
+        Invoke(context: CanvasRenderingContext2D) {
+            context.msFillRule = this._fillRule === FillRule.EvenOdd ? "evenodd" : "nonzero";
+        }
+    }
+
+    class MoveToCommand implements IPathCommand {
+        private _point: Point;
+
+        get Point(): Point { return this._point; }
+
+        constructor(p: Point) {
+            this._point = p;
+        }
+
+        Invoke(context: CanvasRenderingContext2D) {
+            context.moveTo(this._point.X, this.Point.Y);
+        }
+    }
+
+    class LineToCommand implements IPathCommand {
+        private _point: Point;
+
+        get Point(): Point { return this._point; }
+
+        constructor(p: Point) {
+            this._point = p;
+        }
+
+        Invoke(context: CanvasRenderingContext2D) {
+            context.lineTo(this._point.X, this.Point.Y);
+        }
+    }
+
+    class ClosePathCommand implements IPathCommand {
+        constructor() {
+        }
+
+        Invoke(context: CanvasRenderingContext2D) {
+            context.closePath();
+        }
+    }
+
+    class TextStream {
+        private _source: string;
+        private _position: number;
+
+        get Current(): string {
+            return this._position >= this._source.length ? null : this._source[this._position];
+        }
+
+        MoveNext() { return ++this._position < this._source.length; }
+        Reset() { this._position = 0; }
+
+        constructor(source: string) {
+            this._source = source;
+            this.Reset();
+        }
+    }
+
+    export class PathParser {
+        private _wsChars = [" ", "\t"];
+        private _commandChars = ["M", "L", "C", "Q", "A", "Z"];
+
+        private IsCommandCharacter(current: string): boolean {
+            return this._commandChars.indexOf(current) !== -1;
+        }
+
+        private IsWhitespaceCharacter(current: string): boolean {
+            return this._wsChars.indexOf(current) !== -1;
+        }
+
+        private IsComma(current: string): boolean {
+            return current === ",";
+        }
+
+        private IsNumberOrDecimal(current: string) {
+            return !isNaN(parseInt(current, 10)) || current === ".";
+        }
+
+        private SkipWhitespace(stream: TextStream) {
+            while (this.IsWhitespaceCharacter(stream.Current) && stream.MoveNext()) { }
+        }
+
+        private SkipComma(stream: TextStream) {
+            if (this.IsComma(stream.Current)) stream.MoveNext();
+        }
+
+        private ReadNumber(stream: TextStream): number {
+            var numStr = "";
+
+            while (this.IsNumberOrDecimal(stream.Current)) {
+                numStr += stream.Current;
+                if (!stream.MoveNext()) break;
+            }
+
+            return Number(numStr);
+        }
+
+        private ReadPoint(stream: TextStream): Point {
+            var x = this.ReadNumber(stream);
+            this.SkipWhitespace(stream);
+            this.SkipComma(stream);
+            this.SkipWhitespace(stream);
+            var y = this.ReadNumber(stream);
+
+            return new Point(x, y);
+        }
+
+        private ParseFillStyleCommand(stream: TextStream): IPathCommand[] {
+            if (!stream.MoveNext()) throw "Unexpected end of stream while parsing fill style command";
+
+            var fillRule: FillRule;
+
+            switch (this.ReadNumber(stream)) {
+                case 0:
+                    fillRule = FillRule.EvenOdd;
+                    break;
+                case 1:
+                    fillRule = FillRule.NonZero;
+                    break;
+                default:
+                    throw "Invalid fill style option. Valid options are 0 (evenodd) and 1 (nonzero)";
+            }
+
+            this.SkipWhitespace(stream);
+
+            return [new FillRuleCommand(fillRule)];
+        }
+
+        private ParseMoveToCommand(stream: TextStream): IPathCommand[] {
+            if (!stream.MoveNext()) throw "Unexpected end of stream while parsing move to command";
+            this.SkipWhitespace(stream);
+
+            var p = this.ReadPoint(stream);
+
+            this.SkipWhitespace(stream);
+
+            return [new MoveToCommand(p)];
+        }
+
+        private ParseLineToCommand(stream: TextStream): IPathCommand[] {
+            if (!stream.MoveNext()) throw "Unexpected end of stream while parsing line to command";
+            this.SkipWhitespace(stream);
+
+            var commands: IPathCommand[] = [];
+
+            while (!this.IsCommandCharacter(stream.Current) && stream.Current != null) {
+                var p = this.ReadPoint(stream);
+                this.SkipWhitespace(stream);
+                commands.push(new LineToCommand(p));
+            }
+
+            return commands;
+        }
+
+        private ParseClosePathCommand(stream: TextStream): IPathCommand[] {
+            stream.MoveNext();
+            this.SkipWhitespace(stream);
+
+            return [new ClosePathCommand()];
+        }
+
+        private ResolveInitialCommand(stream: TextStream): IPathCommand[] {
+            this.SkipWhitespace(stream);
+
+            switch (stream.Current) {
+                case "F": return this.ParseFillStyleCommand(stream);
+                case "M": return this.ParseMoveToCommand(stream);
+            }
+
+            throw "Invalid command";
+        }
+
+        private ResolveDrawingCommands(stream: TextStream) : IPathCommand[] {
+            switch (stream.Current) {
+                case "M": return this.ParseMoveToCommand(stream);
+                case "L": return this.ParseLineToCommand(stream);
+                case "C": return [];
+                case "Q": return [];
+                case "A": return [];
+                case "Z": return this.ParseClosePathCommand(stream);
+            }
+
+            throw "Invalid command";
+        }
+
+        Parse(context: CanvasRenderingContext2D, path: string): IPathCommand[]{
+            if (context === null) throw "Missing drawing context";
+            if (path === "") return [];
+
+            var stream = new TextStream(path);
+
+            this.ResolveInitialCommand(stream).forEach(c => c.Invoke(context));
+
+            while (true) {
+                this.ResolveDrawingCommands(stream).forEach(c => c.Invoke(context));
+
+                if (stream.Current != null && this.IsCommandCharacter(stream.Current)) continue;
+                if (!stream.MoveNext()) break;
+            }
+
+            context.stroke();
+            //context.fill();
+        }
+    }
+}
